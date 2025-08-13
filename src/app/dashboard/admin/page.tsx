30bc794a71cdffed8211d709f7ab998b4203d1de
Oluwasegun Benjamin Tobi <benjaminsegun38@gmail.com>
2025-08-13 16:14:16 +0100

    feat: Implement Admin Dashboard with User Cards and Charts
    - Added CountChart and UserCard components for displaying user statistics.
    - Created AttendanceChart component for attendance visualization.
    - Updated AdminPage to include UserCards and CountChart.
    - Modified DashboardLayout to change logo text from "SchooLama" to "EduLama".
    - Enhanced tailwind configuration with custom colors for better UI styling.
    - Updated package.json and package-lock.json to include new dependencies (recharts, redux, etc.).

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,7 +1,34 @@
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
 import React from "react";
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className=" flex flex-col gap-4 md:flex-row">
+      {/* Left */}
+      <div className=" w-full lg:w-2/3 flex flex-col gap-8">
+        {/* User Card */}
+        <div className=" flex justify-between gap-4 flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* Middle Chats */}
+        <div className=" flex gap-4 flex-col lg:flex-row">
+          {/* Count Chat */}
+          <div className=" w-full lg:w-1/3 h-[350px]">
+            <CountChart />
+          </div>
+          {/* Attendance Chat */}
+          <div className=" w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom Chat */}
+        <div></div>
+      </div>
+      <div className=" w-full lg:w-1/3">R</div>
+    </div>
+  );
 };
 
 export default AdminPage;
